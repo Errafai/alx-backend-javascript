d9f7f4d3918494bd58f280278a0b953581e78eb3
Errafai <errafaiachraf1923@gmail.com>
2025-08-10 17:19:56 +0100

    3-currency.js 4-pricing.js fix

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -21,7 +21,7 @@ export default class Pricing {
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.name})`;
+    return `${this.amount} ${this.currency.name} (${this.currency.name})`;
   }
 
   static convertPrice(amount, conversionRate) {
