06a6f93aba6acdd2c16d100b8f202be00a1ed019
Errafai <errafaiachraf1923@gmail.com>
2025-08-10 17:26:44 +0100

    3-currency.js fix 2

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -5,14 +5,10 @@ export default class Currency {
   }
 
   get code() {
-    return this._name;
+    return this._code; 
   }
 
   set code(value) {
-    if (typeof (value) !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-
     this._code = value;
   }
 
@@ -21,10 +17,6 @@ export default class Currency {
   }
 
   set name(value) {
-    if (typeof (value) !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-
     this._name = value;
   }
 
