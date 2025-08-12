dd36369b8ec9ec7d52529deb8acd5747b0714f94
Errafai <errafaiachraf1923@gmail.com>
2025-08-12 12:12:11 +0100

    8-hbtn_class.js

--- 0x02-ES6_classes/8-hbtn_class.js
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -1,32 +1,15 @@
-export default class HolbertonClass {
+export default class ALXClass {
   constructor(size, location) {
-    this.size = size;
-    this.location = location;
-  }
-
-  get size() {
-    return this._size;
-  }
-
-  set size(value) {
-    this._size = value;
-  }
-
-  get location() {
-    return this._location;
-  }
-
-  set location(value) {
-    this._location = value;
+    this._size = size;
+    this._location = location;
   }
 
   [Symbol.toPrimitive](hint) {
-    if (hint === 'number') {
-      return this.size;
-    }
     if (hint === 'string') {
-      return this.location;
+      return this._location;
+    } if (hint === 'number') {
+      return this._size;
     }
-    return this;
+    return undefined;
   }
 }
